eb33d19a5d12f4e1e180863ac53321c66090d3f6
gaurav876409 <gaurav876409@gmail.com>
2023-07-17 18:22:45 +0530

    up

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ app.use(express.json());
 // Write a GET Request to get the next number from the input 'num'.
 // Endpoint : /api/get-next-num
 // Return the response as {message : , status: }
+app.get('/api/get-next-num', (req, res) => {
+    const { num } = req.body;
+  
+    if (!Number.isInteger(num)) {
+      return res.status(404).json({
+        message: 'Invalid number provided!',
+        status: 'failure',
+      });
+    }
+  
+    const nextNumber = num + 1;
+  
+    res.status(200).json({
+      message: nextNumber,
+      status: 'success',
+    });
+  });
+  
 
 module.exports = app;
