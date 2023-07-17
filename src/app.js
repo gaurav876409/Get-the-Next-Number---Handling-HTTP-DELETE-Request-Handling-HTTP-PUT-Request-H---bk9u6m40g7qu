0f02e2c55bb1fdae67375ef9f378d019525f5c68
gaurav876409 <gaurav876409@gmail.com>
2023-07-17 21:10:32 +0530

    up

--- src/app.js
+++ src/app.js
@@ -8,23 +8,42 @@ app.use(express.json());
 // Endpoint : /api/get-next-num
 // Return the response as {message : , status: }
 app.get('/api/get-next-num', (req, res) => {
-    const { num } = req.body;
-  
-    if (isNaN(num) || !Number.isInteger(num)) {
-      return res.status(404).json({
-        message: 'Invalid number provided!',
-        status: 'failure',
-      });
-    }
-  
+  const { num } = req.body;
+
+  if (!isNaN(num) && Number.isInteger(num)) {
     const nextNumber = num + 1;
-  
-    res.status(200).json({
+    return res.status(200).json({
       message: nextNumber,
       status: 'success',
     });
+  }
+
+  const nextNumber = num + 1;
+
+  res.status(404).json({
+    message: 'Invalid number provided!',
+    status: 'failure',
   });
-  
+});
+
+// app.get('/api/get-next-num', (req, res) => {
+//     const { num } = req.body;
+
+//     if (isNaN(num) || !Number.isInteger(num)) {
+//       return res.status(404).json({
+//         message: 'Invalid number provided!',
+//         status: 'failure',
+//       });
+//     }
+
+//     const nextNumber = num + 1;
+
+//     res.status(200).json({
+//       message: nextNumber,
+//       status: 'success',
+//     });
+//   });
+
 
 // app.get('/api/get-next-num', (req, res) => {
 //   const { num } = req.body;
