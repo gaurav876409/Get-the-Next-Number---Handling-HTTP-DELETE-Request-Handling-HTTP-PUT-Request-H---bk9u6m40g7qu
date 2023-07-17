7525887f72ca3634a990d52d444fe81324cdfd3a
gaurav876409 <gaurav876409@gmail.com>
2023-07-17 18:37:24 +0530

    up

--- src/app.js
+++ src/app.js
@@ -7,23 +7,48 @@ app.use(express.json());
 // Write a GET Request to get the next number from the input 'num'.
 // Endpoint : /api/get-next-num
 // Return the response as {message : , status: }
+// app.get('/api/get-next-num', (req, res) => {
+//     const { num } = req.body;
+  
+//     if (!Number.isInteger(num)) {
+//       return res.status(404).json({
+//         message: 'Invalid number provided!',
+//         status: 'failure',
+//       });
+//     }
+  
+//     const nextNumber = num + 1;
+  
+//     res.status(200).json({
+//       message: nextNumber,
+//       status: 'success',
+//     });
+//   });
+  
+
+
+// GET endpoint to get the next number from the input 'num'
 app.get('/api/get-next-num', (req, res) => {
-    const { num } = req.body;
-  
-    if (!Number.isInteger(num)) {
-      return res.status(404).json({
-        message: 'Invalid number provided!',
-        status: 'failure',
-      });
-    }
-  
-    const nextNumber = num + 1;
-  
-    res.status(200).json({
-      message: nextNumber,
-      status: 'success',
+  const num = parseInt(req.query.num);
+
+  // Check if the 'num' parameter exists and is a valid integer
+  if (isNaN(num)) {
+    return res.status(400).json({
+      message: 'Invalid number provided!',
+      status: 'failure',
     });
+  }
+
+  // Calculate the next number by incrementing 'num' by 1
+  const nextNumber = num + 1;
+
+  res.status(200).json({
+    message: nextNumber,
+    status: 'success',
   });
-  
+});
+
+module.exports = app;
+
 
 module.exports = app;
